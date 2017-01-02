79e863ff49d6d2ed475d191d3f919f0a63f36317
Bill Crull <atomhrt7@gmail.com>
2017-01-02 18:34:12 +0000

    Added contacts model

--- db/migrate/20161229223701_create_contacts.rb
+++ db/migrate/20161229223701_create_contacts.rb
@@ -1,10 +1,11 @@
 class CreateContacts < ActiveRecord::Migration[5.0]
   def change
     create_table :contacts do |t|
-      t.string :name 
+      t.string :name
       t.string :email
       t.text :comments
-      t.timestamp
+      t.timestamps
     end
   end
 end
+
