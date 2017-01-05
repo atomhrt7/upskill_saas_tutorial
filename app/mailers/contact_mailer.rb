6bd67db23353fee9e073e51751db29e9badded0c
Bill Crull <atomhrt7@gmail.com>
2017-01-05 04:26:22 +0000

    Added contact mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ConatactMailer < ActionMailer::Base
+    default to: 'atomhrt7@aol.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+    end
+end    
